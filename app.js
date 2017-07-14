14cb58a011d2b4308ed584a68b92220e28121eec
Krogul <krogul222@gmail.com>
2017-07-14 14:37:20 +0100

    Basic player support

--- app.js
+++ app.js
@@ -15,29 +15,68 @@ var listener = server.listen(2000, function(){
 console.log("Server started.");
 
 const SOCKET_LIST = {};
+const PLAYER_LIST= {};
+
+let Player = function(id){
+    let self = {
+        x:250,
+        y:250,
+        id:id,
+        number:"" + Math.floor(10*Math.random()),
+        pressingRight: false,
+        pressingLeft: false,
+        pressingUp: false,
+        pressingDown: false,
+        maxSpeed: 10
+     }
+    
+    self.updatePosition = function(){
+        if(self.pressingRight)
+            self.x += self.maxSpeed;
+        if(self.pressingLeft)
+            self.x -= self.maxSpeed;
+        if(self.pressingUp)
+            self.y -= self.maxSpeed;
+        if(self.pressingDown)
+            self.y += self.maxSpeed;
+    }
+    return self;
+}
 
 var io = require('socket.io')(server,{});
 io.sockets.on('connection', function(socket){
     console.log("Socket connection");
     socket.id = Math.random();
-    socket.x = 0;
-    socket.y = 0;
-    socket.number ="" + Math.floor(10*Math.random());
     SOCKET_LIST[socket.id] = socket;
+    
+    var player = Player(socket.id);
+    PLAYER_LIST[socket.id] = player;
     
     socket.on('disconnect',function(){
         delete SOCKET_LIST[socket.id];
+        delete PLAYER_LIST[socket.id];
     });
+    
+    socket.on('keyPress', function(data){
+       if(data.inputId == 'left')
+           player.pressingLeft = data.state;
+       if(data.inputId == 'right')
+           player.pressingRight = data.state;
+       if(data.inputId == 'up')
+           player.pressingUp = data.state;
+       if(data.inputId == 'down')
+           player.pressingDown = data.state;
+    });
+    
     });    
 
 
 setInterval(function(){
     let pack =[];
-    for(let i in SOCKET_LIST){
-        let socket = SOCKET_LIST[i];
-        socket.x++;
-        socket.y++;
-        pack.push({x:socket.x,y:socket.y, number: socket.number});
+    for(let i in PLAYER_LIST){
+        let player = PLAYER_LIST[i];
+        player.updatePosition();
+        pack.push({x:player.x,y:player.y, number: player.number});
     }
     
      for(let i in SOCKET_LIST){
